e04ea4957f05d2aa147a2562c398d572c41d2370
W0lvRine007 <ausaf1325@gmail.com>
2021-11-09 00:48:52 +0530

    second modification

--- catalog_app/lib/main.dart
+++ catalog_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:catalog_app/home_page.dart';
+import 'package:catalog_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
     );
   }
 }
