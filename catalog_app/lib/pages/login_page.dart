9cb5ab7e2fa3ca6dcb6d42e55abf78088060fa05
W0lvRine007 <ausaf1325@gmail.com>
2021-11-10 14:56:35 +0530

    fourth modification

--- catalog_app/lib/pages/login_page.dart
+++ catalog_app/lib/pages/login_page.dart
@@ -6,9 +6,49 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(children: [
-        Image.asset("assets/images/login.png")
-      ],),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 32,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "UserName",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                
+              ],
+            ),
+          ),
+          Text(
+            "Welcome!",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
